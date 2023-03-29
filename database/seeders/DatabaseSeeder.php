03e897799706b2c55b8af3cccf6849dd30abadbf
Vinit <vinitmehta91577@gmail.com>
2023-03-29 16:16:42 +0530

    Attendance merged

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,13 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-<<<<<<< HEAD
 use App\Models\Attendance;
-=======
 use App\Models\Committee;
 use App\Models\Member;
 use App\Models\User;
->>>>>>> f28e61515969cec0f0976b3719b09695a262c8f0
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -27,12 +24,9 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-<<<<<<< HEAD
         Attendance::factory(30)->create();
-=======
         User::factory(30)->create();
         Member::factory(30)->create();
         Committee::factory(10)->create();
->>>>>>> f28e61515969cec0f0976b3719b09695a262c8f0
     }
 }
